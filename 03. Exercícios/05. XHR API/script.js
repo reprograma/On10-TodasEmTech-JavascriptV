0f41bdc173e9479a1dce67b59ae258216b1e703b
Débora Borges <borgesdb@gmail.com>
2021-05-27 20:37:31 -0300

    Primeira parte do exercio com XHR

--- 03. Exercícios/05. XHR API/script.js
+++ 03. Exercícios/05. XHR API/script.js
@@ -1,3 +1,18 @@
 // Fazer requisições utilizando XHR na api documentada em:
 // https://ghibliapi.herokuapp.com/
 // Obter lista de filmes, um filme por id, lista de pessoas e pessoa por id
+
+function loadFilms() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://ghibliapi.herokuapp.com/films", false);
+    //xhr.responseType = "json"
+
+    xhr.onload = function () {
+        const json = JSON.parse(xhr.response)
+        console.log(json)
+    }
+
+    xhr.send();
+}
+
+loadFilms()
