690d8a1f5cfdd112da6662046c5dc256447504c3
Bruna Souza <brunasdesouza98@gmail.com>
2021-05-28 18:19:19 -0300

    resolucao dos exercicios

---
+++ bruna-souza/01. Fetch Imagem/script.js
@@ -0,0 +1,18 @@
+// 1.a. fetch da imagem
+// [] adicionar a tag <script> no html
+// [] verificar se está lendo script.js
+// [] usar o método fetch() para buscar o arquivo meme.jpg
+// [] usar método .then() para transformar o dado recebido
+// [] apresentar <img>
+
+const mostraImagem = fetch("./meme.jpg");
+
+mostraImagem.then(r => r.blob())
+.then(body =>{
+    const blobUrl = URL.createObjectURL(body);
+    const imagemDom = document.getElementById('imagem');
+    imagemDom.src = blobUrl;
+})
+
+mostraImagem(); 
+
